2613259b862668a4e04005abd6a4fbd0d0e34ce4
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-09 00:10:42 -0400

    setup up featured products feature - reducers, actions, productpage

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import styled from 'styled-components'
-import { formatPrice } from '../utils/helpers'
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import styled from 'styled-components';
+import { formatPrice } from '../utils/helpers';
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-const Product = () => {
-  return <h4>product</h4>
-}
+const Product = ({ image, name, id, price }) => {
+  return (
+    <Wrapper>
+      <div className='container'>
+        <img src={image} alt={name} />
+        <Link to={`/products/${id}`} className='link'>
+          <FaSearch />
+        </Link>
+      </div>
+      <footer>
+        <h5>{name}</h5>
+        <p>{formatPrice(price)}</p>
+      </footer>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.article`
   .container {
@@ -51,7 +64,7 @@ const Wrapper = styled.article`
     margin-top: 1rem;
     display: flex;
     justify-content: space-between;
-    align-items: center;
+    align-items: end;
   }
   footer h5,
   footer p {
@@ -60,8 +73,8 @@ const Wrapper = styled.article`
   }
 
   footer p {
-    color: var(--clr-primary-5);
+    color: var(--clr-primary-2);
     letter-spacing: var(--spacing);
   }
-`
-export default Product
+`;
+export default Product;
