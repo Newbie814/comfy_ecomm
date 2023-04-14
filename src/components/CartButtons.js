3248da93b4ce8a77779912db503e35bc46b69bfe
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-14 18:10:36 -0400

    setup imports and state/context in checkout form

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -8,7 +8,7 @@ import { useUserContext } from '../context/user_context';
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
-  const { total_items } = useCartContext();
+  const { total_items, clearCart } = useCartContext();
   const { loginWithRedirect, logout, myUser } = useUserContext();
 
   return (
@@ -32,9 +32,10 @@ const CartButtons = () => {
         <button
           type='button'
           className='auth-btn logout-btn'
-          onClick={() =>
-            logout({ logoutParams: { returnTo: window.location.origin } })
-          }
+          onClick={() => {
+            clearCart();
+            logout({ logoutParams: { returnTo: window.location.origin } });
+          }}
         >
           Logout <FaUserMinus />
         </button>
